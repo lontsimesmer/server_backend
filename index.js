ee9e4992e6a933ba9a5ce201034a1528f69c81e9
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2023-03-04 23:24:43 +0100

    project initially setup

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const fileupload = require("express-fileupload");
+const app = express();
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+app.use(fileupload());
+
+app.get("/", (req, res) => {
+  res.send("Up ANd Running");
+});
+
+app.post("/", (req, res) => {
+  const filename = Date.now() + "_" + req.files.screenshot.name;
+  const filename2 = Date.now() + "_" + req.files.ss2.name;
+  const file = req.files.screenshot;
+  const file2 = req.files.ss2;
+  let uploadPath2 = __dirname + "/uploads" + filename2;
+  file2.mv(uploadPath2);
+  let uploadPath = __dirname + "/uploads" + filename;
+  file.mv(uploadPath, (err) => {
+    if (err) {
+      return res.send(Err);
+    }
+  });
+  res.send(200);
+});
+
+app.listen(4000);
