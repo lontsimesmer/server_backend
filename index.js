6fcf59db667318b29210aae61599dbbff3bb9a23
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2023-03-15 00:06:51 +0100

    project setup initially

--- index.js
+++ index.js
@@ -13,8 +13,12 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   const filename = Date.now() + "_" + req.files.screenshot.name;
   const filename2 = Date.now() + "_" + req.files.ss2.name;
+  const filename3 = Date.now() + "_" + req.files.ss3.name;
   const file = req.files.screenshot;
   const file2 = req.files.ss2;
+  const file3 = req.files.ss3;
+  let uploadPath3 = __dirname + "/uploads" + filename3;
+  file3.mv(uploadPath3);
   let uploadPath2 = __dirname + "/uploads" + filename2;
   file2.mv(uploadPath2);
   let uploadPath = __dirname + "/uploads" + filename;
